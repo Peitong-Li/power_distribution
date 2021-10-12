5557f2ec2d9bf0e89a6aad55ba33612ba3856f10
PeiTong Li <45646492+Peitong-Li@users.noreply.github.com>
2021-10-12 21:17:52 +0800

    Update get_url.js

--- uniApp/default/get_url.js
+++ uniApp/default/get_url.js
@@ -1,3 +1,7 @@
 export const config_url = {
-  baseurl:'http://localhost:8091/power_distribution/'
+  baseurl:'http://192.168.167.230:8091/power_distribution/'
+}
+
+window.configIp = function(IP){
+	config_url.baseurl = "http://" + IP + ":8091/power_distribution/"
 }
