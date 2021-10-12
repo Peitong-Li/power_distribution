6faec8f5c0dd8aee9025107e4dce50415aad67a3
PeiTong Li <45646492+Peitong-Li@users.noreply.github.com>
2021-10-12 21:17:34 +0800

    Create configuration_IP.vue

---
+++ uniApp/pages/mine/configuration_IP.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<input style="background-color:#E5E5E5; border-radius: 10upx;height: 100upx; margin-top: 20upx;" type="text" v-model="IP"/>
+		<button style="width: 50%; margin-top: 10upx;" type="primary" @click="confirmIP">确定</button>
+	</view>
+</template>
+
+<script>
+	import {config_url} from '../../default/get_url.js'
+	export default{
+		data(){
+			return{
+				IP:"请输入配置IP"
+			}
+		},
+		methods: {
+			confirmIP() {
+				configIp(this.IP)
+				console.log(config_url.baseurl)
+				uni.showToast({
+					title: "配置成功！",
+					icon: "success",
+					duration: 2000
+				})
+				uni.switchTab({
+					url: "../index/index"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
